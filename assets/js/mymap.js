244b3b886117c26230409df9b60ef29814429165
mstosio <michaustosio@gmail.com>
2018-09-04 16:48:26 +0200

    Added new markers and some functions

--- assets/js/mymap.js
+++ assets/js/mymap.js
@@ -1,5 +1,5 @@
 
-var mymap = L.map('mymap').setView([52.167930660117555, 22.271411418914795], 15);
+const mymap = L.map('mymap').setView([52.167930660117555, 22.271411418914795], 15);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
@@ -9,7 +9,7 @@ var mymap = L.map('mymap').setView([52.167930660117555, 22.271411418914795], 15)
 		id: 'mapbox.streets'
 	}).addTo(mymap);
 
-
+	
 
 	// importowanie geoJSON przez AJAX call
 
@@ -28,9 +28,19 @@ var mymap = L.map('mymap').setView([52.167930660117555, 22.271411418914795], 15)
 				}
 			},
 			pointToLayer: function(geoJsonPoint, latlng){
-				return  L.marker(latlng, {
-						icon: restaurantIcon
-					});	  
+				if(geoJsonPoint.properties.restaurant === 'kebab'){
+					return  L.marker(latlng, {
+						icon: kebabIcon
+					});
+				} else if (geoJsonPoint.properties.restaurant === 'pizza') {
+					return  L.marker(latlng, {
+						icon: pizzaIcon
+					});
+				} else if (geoJsonPoint.properties.restaurant === 'sushi') {
+					return  L.marker(latlng, {
+						icon: sushiIcon
+					});
+				} 						
 			},
 			onEachFeature: function(feature, layer){
 				if(feature.geometry.type === 'Point'){
@@ -40,7 +50,24 @@ var mymap = L.map('mymap').setView([52.167930660117555, 22.271411418914795], 15)
 						feature.properties.adres
 					  );
 				}
-			}
+			},
+			onEachFeature: function(feature, layer){
+				const toggle = document.getElementById("togglelayer");
+				toggle.addEventListener("click", function(){
+					const kebab = feature.properties.restaurant === 'kebab';
+					const kebabImg = document.querySelectorAll('img[src="assets/mapicons/kebab.png"]');
+					if(kebab){
+						console.log(kebabImg);
+						kebabImg.forEach(function(keba){
+							keba.style.display = "none";
+						})	
+					} else {
+						console.log("retard");
+					}
+				});
+			
+			},
+
 		}).addTo(mymap);
 	})
 	.catch(error => console.log(error.message));
@@ -57,5 +84,25 @@ var mymap = L.map('mymap').setView([52.167930660117555, 22.271411418914795], 15)
 		}
 	});
 
-	const restaurantIcon = new gooportalIcon({iconUrl: 'assets/mapicons/kebab.png'});
+	const kebabIcon = new gooportalIcon({iconUrl: 'assets/mapicons/kebab.png'});
+	const pizzaIcon = new gooportalIcon({iconUrl: 'assets/mapicons/pizza.png'});
+	const sushiIcon = new gooportalIcon({iconUrl: 'assets/mapicons/sushi.png'});
+
+	
+
+	
+	//coordinates 
+
+	// mymap.on('moveend'), function(e){
+	// 	$('#current_center').html(mymap.getCenter().lat+','+mymap.getCenter().lng);
+	// } 
+
+	mymap.on('moveend', function(e){
+		const inputbox = document.getElementById("current_center");
+		const lat = mymap.getCenter().lat.toFixed(7);
+		const lng = mymap.getCenter().lng.toFixed(7);
+		inputbox.value = lat + "," + lng;			
+	})
+
+	
 
